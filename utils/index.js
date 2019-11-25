525633ee5fdf1699583322bd68b590fea7ee4181
hangaoke1 <188869009@qq.com>
2019-11-25 23:02:33 +0800

    feat: update

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,30 @@
-exports.filterCookie = function(cookie) {
-  const whiteList = ['TrackerID', 'pt_key', 'pt_pin', 'pt_token', 'pwdt_id'];
+const request = require('request');
+
+exports.filterJDCookie  = function (cookie) {
+  cookie = cookie.replace(/\s/g, '')
+  const whiteList = ['TrackerID', 'pt_key', 'pt_pin', 'pt_token', 'pwdt_id']
   return cookie.split(';').filter(item => {
-    for(let i = 0; i < whiteList.length; i++) {
+    for (let i = 0; i < whiteList.length; i++) {
       if (item.indexOf(whiteList[i]) > -1) {
         return true
       }
     }
     return false
-  }).join(';').trim() + ';'
+  }).join(';').trim()
+}
+
+// 请求服务
+exports.fetch = function(options) {
+	return new Promise((resolve, reject) => {
+		options.resolveWithFullResponse = true;
+		request(options, (error, response, body) => {
+			if (error) {
+				console.log(error);
+				reject(error);
+			} else {
+				response.body = body;
+				resolve(response);
+			}
+		});
+	});
 }
