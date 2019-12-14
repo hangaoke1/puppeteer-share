90c20eb7203a295c54bbf7d8799ce4a2c1b16216
hangaoke1 <188869009@qq.com>
2019-12-14 23:12:37 +0800

    feat: update

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const child_process = require('child_process');
 
 exports.filterJDCookie  = function (cookie) {
   cookie = cookie.replace(/\s/g, '')
@@ -13,10 +14,13 @@ exports.filterJDCookie  = function (cookie) {
   }).join(';').trim()
 }
 
-// 请求服务
+// 请求服务 request promise版本
 exports.fetch = function(options) {
 	return new Promise((resolve, reject) => {
 		options.resolveWithFullResponse = true;
+		options.strictSSL = false
+		options.followRedirect = false
+		options.maxRedirects = 0
 		request(options, (error, response, body) => {
 			if (error) {
 				console.log(error);
@@ -27,4 +31,45 @@ exports.fetch = function(options) {
 			}
 		});
 	});
+}
+
+// 请求服务 curl promise版本
+exports.curl = function(curl) {
+	return new Promise((resolve, reject) => {
+		child_process.exec(curl, function(err, stdout, stderr) {
+			if (err) {
+				reject(err)
+			} else {
+				resolve(stdout)
+			}
+		});
+	})
+}
+
+exports.safeParse = function(str) {
+	try {
+		return JSON.parse(str)
+	} catch (error) {
+		console.log(error)
+		return null
+	}
+}
+
+exports.safeStringify = function(obj) {
+	try {
+		return JSON.stringify(obj)
+	} catch (error) {
+		console.log(error)
+		return ''
+	}
+}
+
+exports.getCookie = function (cookie, name) {
+	let cookieMap = {}
+	cookie.split(';').forEach(item => {
+		const key = item.split('=')[0]
+		const val = item.split('=')[1]
+		cookieMap[key] = val
+	})
+	return cookieMap[name]
 }
